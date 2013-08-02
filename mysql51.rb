5ebef8012a5d72bfb5bbef2998782c3eb871db44
Nate <nchristensen@deseretdigital.com>
2013-08-02 13:13:48 -0600

    Update MySQL Package To the Latest Version
    Updates the MySQL package that is downloaded to be mysql 5.1.71 instead of 5.1.69, because 5.1.69 is no longer available.

--- mysql51.rb
+++ mysql51.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Mysql51 < Formula
   homepage 'http://dev.mysql.com/doc/refman/5.1/en/'
-  url 'http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/mysql-5.1.69.tar.gz'
+  url 'http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/mysql-5.1.71.tar.gz'
   sha1 'f15d813cbb9b4ba6d72f053e52c29ed9036a4061'
 
   option :universal
